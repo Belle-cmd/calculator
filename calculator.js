87e498f683d600d861692ed178687fc90a19a905
BelleProjects <bautistadana55@gmail.com>
2022-08-21 15:04:34 -0600

    Add code for the calculator functions

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function add(addend1, addend2) {
+	return addend1 + addend2;
+};
+
+function subtract(minuend, subtrahend) {
+	return minuend - subtrahend;
+};
+
+function muliply(multiplicand, multiplier) {
+    return multiplicand *  multiplier;
+}
+
+function divide(dividend, divisor) {
+    return dividend / divisor;
+}
