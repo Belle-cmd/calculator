2040021f0130aa2cbddb027d68251d00b62a04d7
BelleProjects <bautistadana55@gmail.com>
2022-08-21 19:23:33 -0600

    Add style to the calculator and buttons

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # calculator
+
+Artists:
+- back.png by [Dixit Lakhani_02](https://www.flaticon.com/free-icons/erase)
--- calculator.js
+++ calculator.js
@@ -1,3 +1,12 @@
+"use strict";
+
+// DOM Elements
+const btnContainerID = document.getElementById("grid");
+const periodBtnID = document.getElementById("ref-to-period");
+
+
+// all functions
+
 function add(addend1, addend2) {
 	return addend1 + addend2;
 };
@@ -31,4 +40,20 @@ function operate(operator, number1, number2) {
     } else if (operator === "divide") {
         divide(number1, number2);
     }
+}
+
+
+function populateBtnNumbers() {
+    for (let i = 9; i > -1; i--) {
+        const btn = document.createElement("button");
+        btn.textContent = i;
+        btn.classList.add("number");
+        btnContainerID.insertBefore(btn, periodBtnID);  // creates the numbers before the period button
+    }
+}
+
+
+// events
+window.onload = (event) => {
+    populateBtnNumbers();
 }
